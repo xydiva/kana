8ec68a895b06043b30c54206884645fb9631d756
xyDiva <xydiva1@gmail.com>
2019-01-15 21:33:11 +0800

    add katakana

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+// 罗马音
 let roma = [
     'a', 'i', 'u', 'e', 'o',
     'ka', 'ki', 'ku', 'ke', 'ko',
     'sa', 'shi', 'su', 'se', 'so',
-    'ta', 'chi', 'tu', 'te', 'to',
+    'ta', 'chi', 'tsu', 'te', 'to',
     'na', 'ni', 'nu', 'ne', 'no',
-    'ha', 'hi', 'hu', 'he', 'ho',
+    'ha', 'hi', 'fu', 'he', 'ho',
     'ma', 'mi', 'mu', 'me', 'mo',
     'ya', 'yu', 'yo',
     'ra', 'ri', 'ru', 're', 'ro',
     'wa', 'wo',
     'n'
 ]
+// 平假名
 let hiragana = [
     'あ', 'い', 'う', 'え', 'お',
     'か', 'き', 'く', 'け', 'こ',
     'さ', 'し', 'す', 'せ', 'そ',
     'た', 'ち', 'つ', 'て', 'と',
     'な', 'に', 'ぬ', 'ね', 'の',
+    'は', 'ひ', 'ふ', 'へ', 'ほ',
     'ま', 'み', 'む', 'め', 'も',
     'や', 'ゆ', 'よ',
     'ら', 'り', 'る', 'れ', 'ろ',
@@ -24,11 +27,27 @@ let hiragana = [
     'ん'
 ]
 
-let type = 1
+// 片假名
+let katakana = [
+    'ア', 'イ', 'ウ', 'エ', 'オ',
+    'カ', 'キ', 'ク', 'ケ', 'コ',
+    'サ', 'シ', 'ス', 'セ', 'ソ',
+    'タ', 'チ', 'ツ', 'テ', 'ト',
+    'ナ', 'ニ', 'ヌ', 'ネ', 'ノ',
+    'ハ', 'ヒ', 'フ', 'ヘ', 'ホ',
+    'マ', 'ミ', 'ム', 'メ', 'モ',
+    'ヤ', 'ユ', 'ヨ',
+    'ラ', 'リ', 'ル', 'レ', 'ロ',
+    'ワ', 'ヲ',
+    'ン'
+]
+
+let type = 1 // 1=看音写假名 2=平假名读音 3=片假名读音
 
 let $card = $('#card')
 let $byRomaBtn = $('#byRomaBtn')
-let $byKanaBtn = $('#byKanaBtn')
+let $byHiraganaBtn = $('#byHiraganaBtn')
+let $byKatakanaBtn = $('#byKatakanaBtn')
 let $randomBtn = $('#randomBtn')
 
 $card.click(function () {
@@ -41,8 +60,14 @@ $byRomaBtn.click(function () {
     setCard()
 })
 
-$byKanaBtn.click(function () {
+$byHiraganaBtn.click(function () {
     type = 2
+    setCur()
+    setCard()
+})
+
+$byKatakanaBtn.click(function () {
+    type = 3
     setCur()
     setCard()
 })
@@ -53,19 +78,36 @@ $randomBtn.click(function () {
 
 function setCur() {
     if (type === 1) {
-        $byKanaBtn.removeClass('cur')
+        $byHiraganaBtn.removeClass('cur')
+        $byKatakanaBtn.removeClass('cur')
         $byRomaBtn.addClass('cur')
+    }
+    else if (type === 2) {
+        $byRomaBtn.removeClass('cur')
+        $byKatakanaBtn.removeClass('cur')
+        $byHiraganaBtn.addClass('cur')
     }
     else {
         $byRomaBtn.removeClass('cur')
-        $byKanaBtn.addClass('cur')
+        $byHiraganaBtn.removeClass('cur')
+        $byKatakanaBtn.addClass('cur')
     }
 }
 
 function setCard() {
-    let len = type === 1 ? roma.length : hiragana.length
+    let len = roma.length
     let random = Math.floor(Math.random() * len)
-    $card.text(type === 1 ? roma[random] : hiragana[random])
+    let text = ''
+    if (type === 1) {
+        text = roma[random]
+    }
+    else if (type === 2) {
+        text = hiragana[random]
+    }
+    else {
+        text = katakana[random]
+    }
+    $card.text(text)
 }
 
 setCur()
